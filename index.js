dab921e7da6e995955818c7a6571f8c5b1db4f72
connorvm <connormannion24@yahoo.com>
2022-10-18 13:36:51 -0400

    commit tree 61287cc6ed249f94cbcbf2e4d6b25b39d0d4c684

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+// Add your code heree
+/*function submitData(name, email){
+    const newData = {
+        name: name,
+             email: email
+
+    }
+    fetch("http://localhost:3000/users",{
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+    },
+            body: JSON.stringify(newData),
+            
+    })
+    .then ( response => response.json())
+    .then ( data => {
+        document.body.innerHTML = data.id
+    })
+    .catch(error2 => {
+        let message = 'Unauthorized Access';
+        document.body.innerHTML = message;
+    });   
+}g
+*/
+
+function submitData(userNames, userEmails) {
+    const userData = {
+        name: userNames,
+        email: userEmails,
+    }
+        return fetch('http://localhost:3000/users', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+          body: JSON.stringify(userData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.body.innerHTML = data.id
+        })
+        .catch(error => {
+          let message = 'Unauthorized Access';
+          document.body.innerHTML = error;
+       })
+      }
+    
+    
+    
+    
+    
